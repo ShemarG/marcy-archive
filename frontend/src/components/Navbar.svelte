62c92734e9171fafc18cc48d51cb33ed2d4ebe51
Devonte Duncan <devonteduncan202@gmail.com>
2022-11-14 03:32:44 -0500

    Swap to Sveltekit, recreate navbar in sveltekit

--- frontend/src/components/Navbar.svelte
+++ frontend/src/components/Navbar.svelte
@@ -1,35 +1,44 @@
 <script>
-	let showBurger = false
+  let showBurger = false;
+  export let page;
 </script>
+
 <nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
-      <img src="https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/277226378_336398221858699_2845459025042218157_n.png?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=cWxgN_El3XQAX_DS8VX&_nc_ht=scontent-lga3-1.xx&oh=00_AfCe9ZVvoJdQxpCE7CuS9ObWuqhAm1VN5TTN4EVWcCKchg&oe=63753458" width="50" height="50">
+    <a class="navbar-item" href="/">
+      <img
+        src="/marcy-logo-classic.png"
+        width="40"
+        height="45"
+      />
     </a>
-
-    <a on:click={(e) => showBurger = !showBurger } role="button" class={`navbar-burger ${showBurger ? 'is-active' : ''}`} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
+    <div class="navbar-start">
+      <h1>The Marcy Lab School Archive</h1>
+    </div>
+    <a
+      on:click={(e) => (showBurger = !showBurger)}
+      role="button"
+      class={`navbar-burger ${showBurger ? "is-active" : ""}`}
+      aria-label="menu"
+      aria-expanded="false"
+    >
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
     </a>
   </div>
 
-  <div id="navbarBasicExample" class={`navbar-menu ${showBurger ? 'is-active' : ''}`}>
-		<div class="navbar-start">
-			<h1>The Marcy Lab School Archive</h1>
-		</div>
+  <div
+    class={`navbar-menu ${showBurger ? "is-active" : ""}`}
+  >
     <div class="navbar-end">
       <div class="navbar-item">
         <div class="buttons">
-          <a class="button is-primary">
-            <strong>Projects</strong>
-          </a>
-          <a class="button is-light">
-            Documents
-          </a>
-					<a class="button is-light">
-            + New Archive
-          </a>
+          {#if page !== "/projects"}
+            <a href="/projects" class="button is-light">Projects</a>
+          {/if}
+          <a class="button is-light">Documents</a>
+          <a class="button is-primary">+ New Archive</a>
         </div>
       </div>
     </div>
@@ -37,20 +46,55 @@
 </nav>
 
 <style>
+  @keyframes fadeIn {
+  from {
+    opacity: 0;
+    transform: translate3d(-40px, 0, 0);
+  }
+  to {
+    transform: translate3d(0);
+    opacity: 1;
+  }
+}
+
+  .is-primary {
+    background-color: #0a0775;
+  }
+
+  .navbar-menu {
+    animation: fadeIn 0.3s forwards;
+  }
+
+  .navbar-burger {
+    color: #ffffff;
+  }
   .navbar-start {
-		display: flex;
-		align-items: center;
-		color: #ffffff;
-	}
+    display: flex;
+    align-items: center;
+  }
 
-	.navbar {
-		background-color: #0a0775;
-		
-	}
+  .navbar-start h1 {
+    font-size: 0.8em;
+    font-weight: bold;
+    color: #ffffff;
+  }
 
-	.navbar-btns {
-		display: flex;
-		justify-content: space-evenly;
-		width: 20%;
-	}
+  .navbar {
+    background-color: #0a0775;
+  }
+
+  .buttons {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    width: 100%;
+    font-weight: bold;
+  }
+  .button {
+    margin-right: 0.5rem;
+    width: 50%;
+  }
+  
+
+  
 </style>
