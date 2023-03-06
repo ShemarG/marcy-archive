c5ce1a5ddae710f3ab5a3fac01e5c45f66d0b43d
Devonte Duncan <devonteduncan202@gmail.com>
2023-03-06 01:39:41 -0500

    Landing Page v1

---
+++ frontend/src/components/FloatingBoxes.svelte
@@ -0,0 +1,143 @@
+<section class="background-area-wrapper">
+  <div class="area">
+    <ul class="circles">
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <li on:click={() => alert("Made by Devonte & Shemar")} />
+      <li />
+      <li />
+      <li />
+      <li />
+      <li />
+      <li />
+      <li />
+      <li />
+      <li />
+    </ul>
+  </div>
+</section>
+
+<style>
+  .background-area-wrapper {
+    pointer-events: none;
+    z-index: -999;
+  }
+
+  .area {
+    background: #ffffff;
+    background: -webkit-linear-gradient(to left, #ffffff, #e7e7e9);
+    width: 100%;
+    height: 100%;
+    z-index: -999;
+  }
+
+  .circles {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+
+  .circles li {
+    position: absolute;
+    display: block;
+    list-style: none;
+    width: 20px;
+    height: 20px;
+    background: rgba(10, 7, 117, 0.5);
+    animation: animate 25s linear infinite;
+    bottom: -150px;
+  }
+
+  .circles li:nth-child(1) {
+    left: 25%;
+    width: 80px;
+    height: 80px;
+    background: rgba(214, 69, 11, 0.5);
+    animation-delay: 0s;
+    z-index: 800;
+  }
+
+  .circles li:nth-child(2) {
+    left: 10%;
+    width: 20px;
+    height: 20px;
+    animation-delay: 2s;
+    animation-duration: 12s;
+  }
+
+  .circles li:nth-child(3) {
+    left: 70%;
+    width: 20px;
+    height: 20px;
+    animation-delay: 4s;
+  }
+
+  .circles li:nth-child(4) {
+    left: 40%;
+    width: 60px;
+    height: 60px;
+    animation-delay: 0s;
+    animation-duration: 18s;
+  }
+
+  .circles li:nth-child(5) {
+    left: 65%;
+    width: 20px;
+    height: 20px;
+    animation-delay: 0s;
+  }
+
+  .circles li:nth-child(6) {
+    left: 75%;
+    width: 110px;
+    height: 110px;
+    animation-delay: 3s;
+  }
+
+  .circles li:nth-child(7) {
+    left: 35%;
+    width: 150px;
+    height: 150px;
+    animation-delay: 7s;
+  }
+
+  .circles li:nth-child(8) {
+    left: 50%;
+    width: 25px;
+    height: 25px;
+    animation-delay: 15s;
+    animation-duration: 45s;
+  }
+
+  .circles li:nth-child(9) {
+    left: 20%;
+    width: 15px;
+    height: 15px;
+    animation-delay: 2s;
+    animation-duration: 35s;
+  }
+
+  .circles li:nth-child(10) {
+    left: 85%;
+    width: 150px;
+    height: 150px;
+    animation-delay: 0s;
+    animation-duration: 11s;
+  }
+
+  @keyframes animate {
+    0% {
+      transform: translateY(0) rotate(0deg);
+      opacity: 1;
+      border-radius: 0;
+    }
+
+    100% {
+      transform: translateY(-1000px) rotate(720deg);
+      opacity: 0;
+      border-radius: 50%;
+    }
+  }
+</style>
--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -1,5 +1,6 @@
 <script>
   import { link } from "svelte-spa-router";
+  import FloatingBoxes from "../components/FloatingBoxes.svelte";
 
   let pages = {
     LANDING: "/",
@@ -12,18 +13,18 @@
 <section>
   <div class="main-content">
     <div class="main-content-wrapper">
-      <div class="context">
-        <div class="context-wrapper">
-          <h1>Welcome to the Marcy Archive</h1>
+      <div class="hero-text">
+        <div class="hero-text-wrapper">
+          <h1>Welcome to <span class="hero-blue-text">The Marcy Archive</span></h1>
           <p>A place to put those foolish ambitions to rest</p>
           <div class="buttons">
-            <a use:link href={pages.PROJECTS} class="button is-light"
+            <a use:link href={pages.PROJECTS} class="btn darker-btn"
               >View Projects</a
             >
-            <a use:link href={pages.DOCUMENTS} class="button is-light"
+            <a use:link href={pages.DOCUMENTS} class="btn darker-btn"
               >View Documents</a
             >
-            <a use:link href={pages.NEW_ARCHIVE} class="button"
+            <a use:link href={pages.NEW_ARCHIVE} class="btn"
               >Upload Your Own</a
             >
           </div>
@@ -47,21 +48,8 @@
       />
       </div>
     </div>
-    <div class="area">
-      <ul class="circles">
-        <li />
-        <li />
-        <li />
-        <li />
-        <li />
-        <li />
-        <li />
-        <li />
-        <li />
-        <li />
-      </ul>
-    </div>
   </div>
+  <FloatingBoxes/>
 </section>
 
 <style>
@@ -70,194 +58,186 @@
     width: 100%;
     justify-content: center;
     align-items: center;
-    height: 80vh;
+    height: 100%;
+    padding: 0px;
   }
 
   .main-content-wrapper {
     display: flex;
     flex-direction: row;
     width: 100%;
+    height: 80vh;
+    z-index: 999;
   }
 
   .showcase-image {
-    width: 480px;
-    height: 300px;
+    display: none;
+    width: 200px;
+    height: 125px;
     padding: 0px;
     position: absolute;
     transform: skew(-0.04turn, 8deg);
     box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
   }
 
-  .website-showcase .showcase-image:nth-child(1) {
-    bottom: 150px;
-    right: 40px;
+  .website-showcase {
+    width: 100%;
+    display: none;
+    align-items: center;
   }
 
-  .website-showcase .showcase-image:nth-child(2) {
-    bottom: 300px;
-    right: 180px;
-  }
-
-
- 
-
-  .context {
+  .hero-text {
     color: #0a0775;
   }
 
-  .context-wrapper {
-    backdrop-filter: blur(10px);
-    width: 100%;
-    padding: 55px;
+  .hero-text-wrapper {
+    padding: 20px;
     border-radius: 10px;
+  }
+
+  .hero-blue-text {
+    color: #ffffff;
+    -webkit-text-stroke: 4px #0a0775;
+    text-stroke: 4px #0a0775;
   }
 
   .buttons {
     display: flex;
-    width: 100%;
     z-index: 999;
     margin-top: 40px;
+    justify-content: center;
   }
 
-  .button:hover {
+  .btn {
+    width: 100%;
+    text-align: center;
+    border: 2px solid black;
+    border-radius: 5px;
+    font-weight: bold;
+    color: black;
+    padding: 15px;
+    margin: 0px;
+    margin-bottom: 20px;
+  }
+
+  .darker-btn {
+    background-color: rgb(194, 192, 192);
+  }
+
+  .btn:hover {
     box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
       rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
       rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
   }
 
-  .context h1 {
+  .hero-text h1 {
     font-style: normal;
-    font-weight: 500;
+    font-weight: 900;
     line-height: 0.9em;
     letter-spacing: -0.04em;
     text-transform: uppercase;
     text-decoration: none;
-    font-size: 96px;
+    font-size: 3em;
+    text-align: center;
     margin-bottom: 12px;
   }
 
-  .context p {
+  .hero-text p {
     font-style: normal;
     font-weight: 500;
     line-height: 1.2em;
     letter-spacing: 0;
     font-size: 18px;
+    text-align: center;
   }
 
-  .area {
-    background: #ffffff;
-    background: -webkit-linear-gradient(to left, #ffffff, #e7e7e9);
+  @media (min-width: 768px) {
+    .showcase-image {
+      display: block;
+      width: 280px;
+      height: 160px;
+    }
+    .website-showcase {
     width: 100%;
-    height: 100vh;
-    z-index: -1;
-  }
-
-  .circles {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-  }
-
-  .circles li {
-    position: absolute;
-    display: block;
-    list-style: none;
-    width: 20px;
-    height: 20px;
-    background: rgba(10, 7, 117, 0.5);
-    animation: animate 25s linear infinite;
-    bottom: -150px;
-  }
-
-  .circles li:nth-child(1) {
-    left: 25%;
-    width: 80px;
-    height: 80px;
-    animation-delay: 0s;
-  }
-
-  .circles li:nth-child(2) {
-    left: 10%;
-    width: 20px;
-    height: 20px;
-    animation-delay: 2s;
-    animation-duration: 12s;
-  }
-
-  .circles li:nth-child(3) {
-    left: 70%;
-    width: 20px;
-    height: 20px;
-    animation-delay: 4s;
-  }
-
-  .circles li:nth-child(4) {
-    left: 40%;
-    width: 60px;
-    height: 60px;
-    animation-delay: 0s;
-    animation-duration: 18s;
-  }
-
-  .circles li:nth-child(5) {
-    left: 65%;
-    width: 20px;
-    height: 20px;
-    animation-delay: 0s;
-  }
-
-  .circles li:nth-child(6) {
-    left: 75%;
-    width: 110px;
-    height: 110px;
-    animation-delay: 3s;
-  }
-
-  .circles li:nth-child(7) {
-    left: 35%;
-    width: 150px;
-    height: 150px;
-    animation-delay: 7s;
-  }
-
-  .circles li:nth-child(8) {
-    left: 50%;
-    width: 25px;
-    height: 25px;
-    animation-delay: 15s;
-    animation-duration: 45s;
-  }
-
-  .circles li:nth-child(9) {
-    left: 20%;
-    width: 15px;
-    height: 15px;
-    animation-delay: 2s;
-    animation-duration: 35s;
-  }
-
-  .circles li:nth-child(10) {
-    left: 85%;
-    width: 150px;
-    height: 150px;
-    animation-delay: 0s;
-    animation-duration: 11s;
-  }
-
-  @keyframes animate {
-    0% {
-      transform: translateY(0) rotate(0deg);
-      opacity: 1;
-      border-radius: 0;
+    display: flex;
+    align-items: center;
+    }
+    .website-showcase .showcase-image:nth-child(1) {
+      bottom: 8%;
+      right: 10%;
     }
 
-    100% {
-      transform: translateY(-1000px) rotate(720deg);
-      opacity: 0;
-      border-radius: 50%;
+    .website-showcase .showcase-image:nth-child(2) {
+      bottom: 18%;
+      right: 15%;
+    }
+
+    .website-showcase .showcase-image:nth-child(3) {
+      bottom: 28%;
+      right: 20%;
+    }
+
+    .buttons {
+      display: flex;
+      flex-direction: column;
+      margin-top: 60px;
+    }
+    .btn {
+      width: 100%;
+      padding: 25px;
+    }
+    .hero-text h1 {
+      text-align: left;
+      font-size: 4em;
+      width: 10em;
+    }
+    .hero-text-wrapper {
+      text-align: left;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      padding: 35px;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    .showcase-image {
+      width: 320px;
+      height: 180px;
+    }
+    .hero-text h1 {
+      font-size: 4em;
+      width: 10em;
+    }
+
+    .buttons {
+      width: 60%;
+    }
+
+    .main-content {
+      padding: 40px;
+      padding-bottom: 0px;
+    }
+    
+  }
+
+  @media (min-width: 1440px) {
+    .showcase-image {
+      width: 500px;
+      height: 282px;
+    }
+
+    .buttons {
+      width: 50%;
+    }
+    .hero-text h1 {
+      text-align: left;
+      font-size: 4.8em;
+      width: 8em;
+    }
+    .main-content {
+      padding: 80px;
+      padding-bottom: 0px;
     }
   }
 </style>
