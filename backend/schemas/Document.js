bec38f092ca2270b5137a559b92c01ed8241c5b9
Shemar Gordon <shgordon@mac-mini.mynetworksettings.com>
2023-03-12 13:51:31 -0400

    documents can be files or links

--- backend/schemas/Document.js
+++ backend/schemas/Document.js
@@ -1,17 +1,29 @@
 let mongoose = require('mongoose')
-let Schema = mongoose.Schema
+let { Schema } = mongoose;
+
+// Discriminators offer a way for documents to have variations but still be considered
+// the same type. Here, FileDocument and LinkDocument are still instances of Document.
+let options = { discriminatorKey: 'document_type' }
 
 let DocumentSchema = new Schema({
   title: String,
   cohort: {location: String, start: String, year: Number},
   authors: Array,
-  document: Buffer,
-  document_type: String,
-  assignment: String
-})
+  document_type: { type: String, required: true }
+}, options)
 
 DocumentSchema.pre(['save', 'findOneAndUpdate'], function (){ 
   if (this.authors && this.authors.length) this.authors.sort()
 })
 
-module.exports = mongoose.model('Document', DocumentSchema)
+const Document = mongoose.model('Document', DocumentSchema)
+
+const FileDocument = Document.discriminator('file',
+  new Schema({ file: { type: Buffer, required: true } }, options)
+)
+const LinkDocument = Document.discriminator('link',
+  new Schema({ link: { type: String, required: true } }, options)
+)
+
+
+module.exports = Document
